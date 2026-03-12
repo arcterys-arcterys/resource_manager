7e5ab749b3c8b2ec4bcfd92d4fc7989e6651a2cb
Anna (Sophie) <33362441+arcterys-arcterys@users.noreply.github.com>
2026-03-12 13:11:54 -0700

    Delete vite.config.js

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// Replace 'resource-manager' with your actual GitHub repository name
-export default defineConfig({
-  plugins: [react()],
-  base: '/resource-manager/',
-})
